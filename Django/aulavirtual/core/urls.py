5e8d1ad483e6c2b90e8fb63aba9ca5420555d71d
Luciano Pueyo <l.pueyo@bue.edu.ar>
2023-09-07 10:06:21 -0300

    Clase 11: Views 2

--- Django/aulavirtual/core/urls.py
+++ Django/aulavirtual/core/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('alumnos/listado', views.alumnos_listado, name='alumnos_listado'),
+    path('alumnos/detalle/<str:nombre_alumno>', views.alumno_detalle, name='alumnos_detalle'),
+    path('alumnos/historico/2017/', views.alumnos_historico_2017, name='alumnos_historico'),
+    re_path(r'alumnos/historico/(?P<year>[0-9]{4})/$', views.alumnos_historico, name='alumnos_historico'),
+    path('alumnos/activos', views.alumnos_estado, {'estado': 'activo'}, name="alumnos_activos"),
+    path('alumnos/inactivos', views.alumnos_estado, {'estado': 'inactivo'}, name="alumnos_inactivos"),
 ]
--- Django/aulavirtual/core/views.py
+++ Django/aulavirtual/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hola Mundo parte 2")
+    return render(request, "index.html")
 
 def alumnos_listado(request):
     return HttpResponse("""
@@ -12,3 +12,20 @@ def alumnos_listado(request):
         <li>Gaston Gomez</li>
     </ul>
     """)
+
+def alumno_detalle(request, nombre_alumno):
+    return HttpResponse(
+        f"""
+        <h1>Bienvenid@ {nombre_alumno} </h1>
+        <p>Pagina Personal de usuario</p>
+        """
+    )
+
+def alumnos_historico(request, year):
+    return HttpResponse(f'<h1>Historico de Alumnos del año: {year}</h1>')
+
+def alumnos_historico_2017(request):
+    return HttpResponse('<h1>Historico de Alumnos del primer año de el Aula Virtual(2017)</h1>')
+
+def alumnos_estado(request, estado):
+    return HttpResponse(f'Filtrar alumnos por estado: {estado}')
