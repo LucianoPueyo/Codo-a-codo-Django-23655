e63174aa7a1e09f0b89bd2660321b358df2da86b
Luciano Pueyo <l.pueyo@bue.edu.ar>
2023-10-12 10:02:50 -0300

    Clase 22: Modelos 2

--- Django/aulavirtual/core/models.py
+++ Django/aulavirtual/core/models.py
@@ -7,3 +7,30 @@ class Persona(models.Model):
     apellido = models.CharField(max_length=30, verbose_name="Apellido")
     email = models.EmailField(max_length=150, verbose_name="Email") 
     dni = models.IntegerField(verbose_name="Dni")
+
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=30, verbose_name="Nombre")
+    apellido = models.CharField(max_length=30, verbose_name="Apellido")
+    email = models.EmailField(max_length=150, verbose_name="Email") 
+    dni = models.IntegerField(verbose_name="Dni")
+    legajo = models.IntegerField(verbose_name="Legajo")
+
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=150, verbose_name="Nombre")
+
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=150, verbose_name="Nombre")
+    descripcion = models.CharField(max_length=150, null=True, verbose_name="Descripcion del curso") 
+    fecha_inicio = models.DateField(verbose_name="Fecha de inicio")
+    cantidad_clases = models.IntegerField(verbose_name="Cantidad de clases")
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    estudiantes = models.ManyToManyField(Estudiante, through="Inscripcion")
+
+
+class Inscripcion(models.Model):
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    fecha = models.DateField(verbose_name="Fecha de inscripcion")
