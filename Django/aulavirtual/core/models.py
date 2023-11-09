19a6376f73ca9be4339e44a32f251986d785ea5a
Luciano Pueyo <l.pueyo@bue.edu.ar>
2023-11-09 10:12:49 -0300

    Clase 29: Auth 2

--- Django/aulavirtual/core/models.py
+++ Django/aulavirtual/core/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 # Create your models here.
+
+
+# Usando un modelo Proxy ya que no agrego nuevos campos
+class Persona2(User):
+    class Meta:
+        proxy = True
 
 class Persona(models.Model):
     nombre = models.CharField(max_length=30, verbose_name="Nombre")
--- Django/aulavirtual/core/urls.py
+++ Django/aulavirtual/core/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, re_path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='core/login.html'),name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+
     path('contacto', views.contacto, name="contacto"),
     path('alumnos/alta', views.alta_alumno, name="alta_alumno"),
     path('alumnos/listado', views.alumnos_listado, name='alumnos_listado'),
